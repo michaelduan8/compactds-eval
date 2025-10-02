e64fcd25bfe3a18e81f191f4ef072f938a6a4766
Mic Dun <micdun@Mics-MacBook-Pro.local>
2025-10-01 20:17:47 -0700

    logs

--- src/postprocessing/rewrite_retrieved_docs.py
+++ src/postprocessing/rewrite_retrieved_docs.py
@@ -9,6 +9,7 @@ from vllm import LLM, SamplingParams
 
 
 def _prepare_doc(doc, trace_key, mode="simple"):
+    print(doc.keys())
     retrieval_text = doc[trace_key]
     question = retrieval_text.split("Reasoning Snippet:")[0].split("Question:")[1].strip()
     reasoning_snippet = retrieval_text.split("Reasoning Snippet:")[1].split("Answer:")[0].strip()
